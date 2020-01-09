0a606227e55fc1ce15de7a334522096a154c69c1
Jason Wong <59460466+jaucifer@users.noreply.github.com>
2020-01-09 14:44:43 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 class Calculator(self):
     def add(first_term, second_term):
         return first_term + second_term
-  
-    def subtract(first_term, second_term)
+
+    def subtract(first_term, second_term):
         return first_term - second_term
