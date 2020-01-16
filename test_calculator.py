14f17e6b261a4121e89c564f168393a39dc9cfd6
Jason Wong <59460466+jaucifer@users.noreply.github.com>
2020-01-16 10:43:47 +0800

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -3,7 +3,7 @@ import calculator
 
 class TestCalculator:
 
-    def test_addition(selfwrong):
+    def test_addition(self):
         assert 4 == calculator.add(2, 2)
 
     def test_subtraction(self):
